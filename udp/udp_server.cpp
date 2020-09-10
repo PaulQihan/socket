3951bcf9670e6d5c53ed9644805670691cec2335
PaulQihan <dsxhqh363279016@sjtu.edu.cn>
2020-09-10 15:54:43 +0800

    udp_client

--- udp/udp_server.cpp
+++ udp/udp_server.cpp
@@ -1,61 +1,72 @@
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <stdio.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <errno.h>
 #include <string.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/shm.h>
-#include <thread>
-#include <iostream>
-#define PORT 8000
-#define BUFFER_SIZE 1024
+
+#define SERV_PORT   8000
 
 int main()
 {
-    int ss = socket(AF_INET, SOCK_STREAM, 0);//若成功则返回一个sockfd（套接字描述符）
-    struct sockaddr_in server_sockaddr;
+  /* sock_fd --- socket文件描述符 创建udp套接字*/
+  int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(sock_fd < 0)
+  {
+    perror("socket");
+    exit(1);
+  }
 
-    server_sockaddr.sin_family = AF_INET;
-    server_sockaddr.sin_port = htons(PORT);//将一个无符号短整型数值转换为网络字节序，即大端模式(big-endian)　
-    //printf("%d\n",INADDR_ANY);
-    //INADDR_ANY就是指定地址为0.0.0.0的地址，这个地址事实上表示不确定地址，或“所有地址”、“任意地址”。
-    //一般来说，在各个系统中均定义成为0值。
-    server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);//将主机的无符号长整形数转换成网络字节顺序。
-    int len = sizeof(server_sockaddr);
-    if(bind(ss, (struct sockaddr* ) &server_sockaddr, sizeof(server_sockaddr))==-1)
+  /* 将套接字和IP、端口绑定 */
+  struct sockaddr_in addr_serv;
+  int len;
+  memset(&addr_serv, 0, sizeof(struct sockaddr_in));  //每个字节都用0填充
+  addr_serv.sin_family = AF_INET;
+  addr_serv.sin_port = htons(SERV_PORT);
+  /* INADDR_ANY表示不管是哪个网卡接收到数据，只要目的端口是SERV_PORT，就会被该应用程序接收到 */
+  addr_serv.sin_addr.s_addr = htonl(INADDR_ANY);  //自动获取IP地址
+  len = sizeof(addr_serv);
+
+  /* 绑定socket */
+  if(bind(sock_fd, (struct sockaddr *)&addr_serv, sizeof(addr_serv)) < 0)
+  {
+    perror("bind error:");
+    exit(1);
+  }
+
+
+  int recv_num;
+  int send_num;
+  char send_buf[20] = "i am server!";
+  char recv_buf[20];
+  struct sockaddr_in addr_client;
+
+  while(1)
+  {
+    printf("server wait:\n");
+
+    recv_num = recvfrom(sock_fd, recv_buf, sizeof(recv_buf), 0, (struct sockaddr *)&addr_client, (socklen_t *)&len);
+
+    if(recv_num < 0)
     {
-        perror("bind");
-        exit(1);
+      perror("recvfrom error:");
+      exit(1);
     }
 
-    char sendbuf[BUFFER_SIZE];
-    char recvbuf[BUFFER_SIZE];
-    struct sockaddr_in client_sockaddr;
+    printf("server receive %s\n", recv_buf);
 
-    while(fgets(sendbuf, sizeof(sendbuf), stdin) != NULL)
+    send_num = sendto(sock_fd, send_buf, recv_num, 0, (struct sockaddr *)&addr_client, len);
+
+    if(send_num < 0)
     {
-        printf("server wait:\n");
-        int recv_num =recvfrom(ss, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *)&client_sockaddr, (socklen_t *)&len);
-
-        if(recv_num < 0)
-        {
-            perror("recv_error");
-            exit(1);            
-        }
-
-        recvbuf[recv_num] = '\0';
-        printf("server receive %d bytes: %s\n", recv_num, recvbuf);  
-        
-        int send_num = sendto(ss, sendbuf, recv_num, 0, (struct sockaddr *)&client_sockaddr, (socklen_t *)&len);
-        if(send_num < 0)
-        {
-            perror("send_error");
-            exit(1);            
-        }
+      perror("sendto error:");
+      exit(1);
     }
-    close(ss);//关闭socket套接字描述符
-    return 0;
+  }
+
+  close(sock_fd);
+
+  return 0;
 }
